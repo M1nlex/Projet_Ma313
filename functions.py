095fd6d20050886be604128e3a8a5d5225644f66
M1nlex <aleandrepe@gmail.com>
2021-01-03 15:54:11 +0100

    oui

--- functions.py
+++ functions.py
@@ -11,3 +11,26 @@ def ResolMCQr(A,b):
 
 def ResolMCNP(A,b):
     pass
+
+def DecompositionGS(A):
+    """ Calcul de la décomposition QR de A une matrice carrée.
+    L'algorithme de Gram-Schmidt est utilisé.
+    La fonction renvoit (Q,R) """
+    n,m=A.shape
+    if n < m :
+        raise Exception('Matrice mal dimensionnée')
+
+    Q=np.zeros((n,m))
+    R=np.zeros((n,m))
+    for j in range(n):
+        for i in range(j):
+            R[i,j]=Q[:,i]@A[:,j]
+        w=A[:,j]
+        for k in range(j):
+            w=w-R[k,j]*Q[:,k]
+        norme=np.linalg.norm(w)
+        if norme ==0:
+            raise Exception('Matrice non inversible')
+        R[j,j]=norme
+        Q[:,j]=w/norme
+    return Q,R
