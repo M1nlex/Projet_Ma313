6c75fe7da3e12913b0914118d1520b0659283fb2
M1nlex <aleandrepe@gmail.com>
2021-01-03 15:37:27 +0100

    oui

--- functions.py
+++ functions.py
@@ -1,1 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def ResolMCEN(A,b):
+    aa = A.T@A
+    bb = A.T@b
+    return np.linalg.solve(aa,bb)
+
+def ResolMCQr(A,b):
+    pass
+
+def ResolMCNP(A,b):
+    pass
