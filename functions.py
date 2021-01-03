95b74f37f5ccd3b369f6ba56a9d46e078cc9e958
rabonarca <romaric.dubois17062000@gmail.com>
2021-01-03 15:37:54 +0100

    Add Cholesky

--- functions.py
+++ functions.py
@@ -1,1 +1,31 @@
 import numpy as np
+
+
+
+def Cholesky(A):
+    """
+    Fonction qui calcule L la matrice de la décomposition de
+    Cholesky de A une matrice réelle symétrique définie positive
+    (A=LL^T où L est triangulaire inférieure).
+    La fonction ne vérifie pas que A est symétrique.
+    La fonction rend L.
+    """
+
+    n,m=A.shape
+    if n != m:
+        raise Exception('Matrice non carrée')
+    L=np.zeros((n,n))
+    for i in range(n):
+        s=0.
+        for j in range(i):
+            s=s+L[i,j]**2
+        R=A[i,i]-s
+        if R<=0:
+            raise Exception('Matrice non définie positive')
+        L[i,i]=np.sqrt(R)
+        for j in range(i+1,n):
+            s=0.
+            for k in range(i):
+                s=s+L[i,k]*L[j,k]
+            L[j,i]=(A[j,i]-s)/L[i,i]
+    return L
